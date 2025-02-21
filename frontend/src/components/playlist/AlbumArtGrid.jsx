6f16aa19f581f29dbf859fe58d6976312448c754
Aaron <gatorspeck1@gmail.com>
2025-02-20 22:08:25 -0600

    clunky implementation of 4x4 album grid as playlist thumbnail

---
+++ frontend/src/components/playlist/AlbumArtGrid.jsx
@@ -0,0 +1,23 @@
+import '../../styles/AlbumArtGrid.css';
+
+export const AlbumArtGrid = ({artList}) => {
+    let gridClass = "grid1x1";
+    if (artList.length > 1) {
+        gridClass = "grid2x2";
+    }
+    else if (artList.length > 4) {
+        gridClass = "grid3x3";
+    }
+
+    return (
+        <div class={gridClass}>
+            {artList.map((art, index) => (
+                <div key={index} className="album-art" style={{ borderRadius: 0 }}>
+                    <img src={art} alt="Album Art" />
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default AlbumArtGrid;
--- frontend/src/repositories/LastFMRepository.jsx
+++ frontend/src/repositories/LastFMRepository.jsx
@@ -67,6 +67,34 @@ export class LastFMRepository {
             throw new Error('Failed to fetch track information');
         }
     }
+
+    // fetch up to 1, 4, or 9 album thumbnails for the playlist
+    async generatePlaylistThumbnail(artistAlbums) {
+        try {
+            const albumThumbnails = await Promise.all(
+                artistAlbums.map(async (album) => {
+                    const albumArt = await this.fetchAlbumArt(album.artist, album.album);
+                    return albumArt;
+                })
+            );
+            
+            if (albumThumbnails.length > 9) {
+                return albumThumbnails.slice(0, 9);
+            }
+            else if (albumThumbnails.length > 4) {
+                return albumThumbnails.slice(0, 4);
+            }
+            else if (albumThumbnails.length >= 1) {
+                return albumThumbnails[0];
+            }
+            else {
+                return null;
+            }
+        }
+        catch (error) {
+            console.error('Error fetching album thumbnails:', error);
+        }
+    }
 };
 
 const lastFMRepository = new LastFMRepository();
