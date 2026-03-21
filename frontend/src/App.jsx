be5b5dfb1995abe53196ac68da51bbb5d0aed06b
Aaron <gatorspeck1@gmail.com>
2026-03-21 10:08:00 -0500

    initial phases of tailwind migration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Playlists from './components/main/Playlists';
 import JobNotifications from './components/job/JobNotifications';
-import './App.css';
+import DarkModeToggle from './components/common/DarkModeToggle';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Playlists />} />
-        <Route path="/playlist/:playlistName" element={<Playlists />} />
-      </Routes>
-      <JobNotifications />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <div className="min-h-screen bg-surface text-text dark:bg-surface-dark dark:text-text-dark">
+          <div className="fixed right-4 top-4 z-50">
+            <DarkModeToggle />
+          </div>
+          <Routes>
+            <Route path="/" element={<Playlists />} />
+            <Route path="/playlist/:playlistName" element={<Playlists />} />
+          </Routes>
+          <JobNotifications />
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
